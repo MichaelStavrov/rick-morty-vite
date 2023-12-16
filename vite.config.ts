59f862ff3a93560aa730b6662e5ef7d8b4be7dcd
Michael <mihali5@mail.ru>
2023-12-16 09:19:09 +0300

    feat: сделал pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,77 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  devOptions: {
+    enabled: true,
+    type: 'module',
+  },
+  registerType: 'prompt',
+  includeAssets: ['**/*'],
+  manifest: {
+    name: 'Rick and Morty',
+    short_name: 'Rick and Morty',
+    icons: [
+      {
+        src: '/public/icons/icon-48x48.png',
+        sizes: '48x48',
+        type: 'image/png',
+      },
+      {
+        src: '/public/icons/icon-72x72.png',
+        sizes: '72x72',
+        type: 'image/png',
+      },
+      {
+        src: '/public/icons/icon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/public/icons/icon-128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+      },
+      {
+        src: '/public/icons/icon-144x144.png',
+        sizes: '144x144',
+        type: 'image/png',
+      },
+      {
+        src: '/public/icons/icon-152x152.png',
+        sizes: '152x152',
+        type: 'image/png',
+      },
+      {
+        src: '/public/icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+      {
+        src: '/public/icons/icon-384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/public/icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    theme_color: '#d9e4f0',
+    background_color: '#d9e4f0',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  base: './',
+  plugins: [react(), tsconfigPaths(), VitePWA(manifestForPlugin)],
 });
